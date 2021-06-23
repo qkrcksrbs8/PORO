e4d3ab76f746acd492472fc6094fffffd4864a0d
ParkChanGyun <qkrcksrbs8@naver.com>
2021-06-23 21:10:45 +0900

    포로스톤 mvc 구조

--- porostone/src/main/java/kr/co/porostone/contents/board/controllers/BoardController.java
+++ porostone/src/main/java/kr/co/porostone/contents/board/controllers/BoardController.java
@@ -1,29 +1,15 @@
 package kr.co.porostone.contents.board.controllers;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+
 import java.util.Locale;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 
@@ -37,12 +23,13 @@ public class BoardController {
 	}
 	
 	@RequestMapping(value = "/poroBoard", method = { RequestMethod.GET, RequestMethod.POST })
-	public String poroBoard(Locale locale, Model model, HttpSession session) {
+	public ModelAndView poroBoard(Locale locale, Model model, HttpSession session) {
 		logger.info("====================================START====================================");
-		logger.info("SESS_GUID = ["+session.getAttribute("TRS_NO")+"], [boardList]"); 
+		logger.info("TRS_NO = ["+session.getAttribute("TRS_NO")+"], [boardList]"); 
+		ModelAndView  mav = new ModelAndView("contents/board/poroBoard.tiles");//연간스케쥴 model
 
 		logger.info("====================================E N D====================================");
-		 return "contents/board/poroBoard.tiles";
+		return mav;
 	}
 
 	
